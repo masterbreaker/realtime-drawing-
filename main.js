d04d237dbc204bc2a406d609261c6fdfb491ddee
Moradabad <75120768+masterbreaker@users.noreply.github.com>
2022-12-04 12:33:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+noseX=0; 
+noseY=0;
+
+leftWristX=0
+rightWristX=0;
+difference=0
+
+function setup(){
+
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    canvas=createCanvas(550,550);
+    canvas.position(560,150);
+    
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function draw(){
+    background("#03adfc");
+    document.getElementById("square_size").innerHTML="width and height of the square will be= "+difference+"px";
+fill("#b03030");
+stroke("#802021");
+square(noseX,noseY,difference);
+}
+ 
+function modelLoaded(){
+    console.log("posenet is initialized");
+
+}
+function gotPoses(results){
+    if(results.length>0)
+    {
+        console.log(results);
+      noseX=results[0].pose.nose.x;
+      noseY=results[0].pose.nose.y;
+      console.log("noseX= "+ noseX+ " noseY= " + noseY);
+leftWristX=results[0].pose.leftWrist.x;
+rightWristX=results[0].pose.rightWrist.x;
+difference=floor(leftWristX-rightWristX);
+    }
+}
